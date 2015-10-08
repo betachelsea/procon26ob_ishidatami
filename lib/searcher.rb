2cf8e2cd170f280402b55ec3d1708b61b92f447f
beta_chelsea <beta.chelsea@gmail.com>
2015-10-08 22:57:45 +0900

    Check candidate other stones

--- lib/searcher.rb
+++ lib/searcher.rb
@@ -61,6 +61,7 @@ class Searcher
     deploy_y = nil
     deploy_side = 'H'
     deploy_rotate = 0
+    stone_candidates = []
 
     # さがす
     ['H', 'T'].each do |side|
@@ -68,24 +69,32 @@ class Searcher
         (-7..38).each do |x|
           (-7..38).each do |y|
             new_score = field.getScore(x, y, side, rotate, stone)
-            if deploy_score < new_score
-              deploy_x = x
-              deploy_y = y
-              deploy_side = side
-              deploy_rotate = rotate
-              deploy_score = new_score
+            if 0 < new_score
+              stone_candidates.push({
+                x: x, y: y, side: side, rotate: rotate,
+                stone_id: stone.id, score: new_score })
             end
           end
         end
       end
     end
 
-    return false if deploy_score == -1
-    # デプロイ
-    field.setCellStatus(deploy_x, deploy_y, deploy_side, deploy_rotate, stone, CellStatus::STONE)
-    stone.setStatus(deploy_x, deploy_y, deploy_side, deploy_rotate)
-    # puts "Score: #{deploy_score}, stone.id=#{stone.id}"
-    return true
+    # 配置候補の中からベストなものを探す
+    deployed = false
+    stone_candidates.sort! { |a, b| b[:score] <=> a[:score] }
+    stone_candidates.each do |status|
+      field.setCellStatus(status[:x], status[:y], status[:side], status[:rotate], stone, CellStatus::PRESTONE)
+      if field.hasAloneCell?
+        field.setCellStatus(status[:x], status[:y], status[:side], status[:rotate], stone, CellStatus::EMPTY)
+      else
+        deployed = true
+        field.setCellStatus(status[:x], status[:y], status[:side], status[:rotate], stone, CellStatus::STONE)
+        stone.setStatus(status[:x], status[:y], status[:side], status[:rotate])
+        break
+      end
+    end
+
+    return deployed
   end
 
   def exportAnswer(stones)
